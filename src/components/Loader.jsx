67b77ca4289b8c25844fea95fbb87cafb7db183c
Abubakar Sadiq <sadiqmuhammad2101@gmail.com>
2024-11-14 01:00:02 -0800

    Add reusable components (Button, InputField, Card, Loader, Modal)

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='Loader'>
+      Loading...
+    </div>
+  )
+}
+
+export default Loader
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../components/Button';
+import InputField from '../components/InputField';
+
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    //login logic here
+    alert(`Logging as ${username}`);
+  } 
+
   return (
     <div>
-      Login
+      <h2>Login</h2>
+      <InputField 
+        type='text'
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <InputField 
+        type='password'
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button text="Login" onClick={handleLogin}/>
     </div>
   )
 }
